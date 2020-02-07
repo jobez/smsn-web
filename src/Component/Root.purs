7b81904a2174b4abd7ec1212256106d45aa5fab9
Johann Makram Salib Bestowrous <johann.bestowrous@gmail.com>
2020-02-06 19:39:36 -0800

    mountains are frustratedly mountains, apparently

--- src/Component/Root.purs
+++ src/Component/Root.purs
@@ -4,32 +4,57 @@ module SmsnWeb.Component.Root
   , Query(..)
   , Action(..)
   -- , Input
-  , Output
+  , Message
+  , wsSender
+  , wsProducer
+  , wsConsumer
   , component
   ) where
 
 import Prelude
 
+import Control.Coroutine as CR
+import Control.Coroutine.Aff (emit)
+import Control.Coroutine.Aff as CRA
+import Control.Monad.Except (runExcept)
 import Control.Monad.Reader (class MonadAsk)
+import Control.Monad.Reader.Trans (ask)
+import Data.Argonaut (class EncodeJson, encodeJson, jsonEmptyObject, stringify, (:=), (~>))
 import Data.Array as A
-import Data.Either (hush)
 import Data.Const (Const)
+import Data.Either (either, hush)
+import Data.Foldable (for_)
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Symbol (SProxy(..))
 import Effect.Aff (Aff)
 import Effect.Aff.Class (class MonadAff)
+import Effect.Class (class MonadEffect)
+import Foreign (F, Foreign, readString, unsafeToForeign)
 import Halogen (HalogenIO, liftEffect)
 import Halogen as H
 import Halogen.HTML as HH
-import Halogen.HTML.Properties as HP
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 import Routing.Duplex (parse)
 import Routing.Hash (getHash)
+import SmsnWeb.Capability.Logging (class Logging, logDebug, logMessage)
 import SmsnWeb.Capability.WebsocketM (class WebsocketM, connect)
 import SmsnWeb.Env (Env)
+import Web.Event.Event (Event)
+import Web.Event.Event as Event
+import Web.Event.EventTarget as EET
+import Web.Socket.Event.EventTypes as WSET
+import Web.Socket.Event.MessageEvent as ME
+import Web.Socket.WebSocket as WS
 
 
-type IO = HalogenIO Query Output Aff
+type IO = HalogenIO Query Message Aff
+
+type Slot = H.Slot Query Message
+
+data Query a = ReceiveMessage String a
+
+data Message = OutputMessage String
 
 type State =
   { messages :: Array String
@@ -41,12 +66,81 @@ type State =
 --   }
 
 -- data Query a = Navigate Route a
-data Query a = ReceiveMessage String a
 
-data Action = Initialize | Connect String
+newtype Args = Args {
+  gremlin :: String
+  , language :: String
+    }
+newtype WSMsg = WSMsg
+  { id :: String
+  , op :: String
+  , processor :: String
+  , args :: Args
+  }
+
+instance encodeArgs :: EncodeJson Args where
+  encodeJson (Args args)
+     = "gremlin" := args.gremlin
+    ~> "language" := args.language
+    ~> "bindings" := jsonEmptyObject
+
+instance encodeWSMsg :: EncodeJson WSMsg where
+  encodeJson (WSMsg msg)
+     = "requestId" := msg.id
+    ~> "op" := msg.op
+    ~> "processor" := msg.processor
+    ~> "args" := msg.args
+    ~> jsonEmptyObject
+
+-- A producer coroutine that emits messages that arrive from the websocket.
+wsProducer :: WS.WebSocket -> CR.Producer String Aff Unit
+wsProducer socket = CRA.produce \emitter -> do
+  listener <- EET.eventListener \ev -> do
+    for_ (ME.fromEvent ev) \msgEvent ->
+      for_ (readHelper readString (ME.data_ msgEvent)) \msg ->
+        emit emitter msg
+  EET.addEventListener
+    WSET.onMessage
+    listener
+    false
+    (WS.toEventTarget socket)
+  where
+    readHelper :: forall a b. (Foreign -> F a) -> b -> Maybe a
+    readHelper read =
+      either (const Nothing) Just <<< runExcept <<< read <<< unsafeToForeign
+
+-- A consumer coroutine that takes the `query` function from our component IO
+-- record and sends `ReceiveMessage` queries in when it receives inputs from the
+-- producer.
+wsConsumer :: (forall a. Query a -> Aff (Maybe a)) -> CR.Consumer String Aff Unit
+wsConsumer query = CR.consumer \msg -> do
+  void $ query $ H.tell $ ReceiveMessage msg
+  pure Nothing
+
+-- A consumer coroutine that takes output messages from our component IO
+-- and sends them using the websocket
+
+wsSender :: WS.WebSocket -> CR.Consumer Message Aff Unit
+wsSender socket = CR.consumer \msg -> do
+  case msg of
+    OutputMessage msgContents ->
+      liftEffect $ WS.sendString socket $ stringify gremlinJson
+      where gremlinMsg = WSMsg {id: "",
+                                op: "eval",
+                                processor: "",
+                                args: argz}
+            argz = Args {gremlin: msgContents, language: "gremlin-groovy"}
+            gremlinJson = encodeJson gremlinMsg
+  pure Nothing
+
+
+data Action = Initialize
+            | HandleInput String
+            | Connect String
+            | Submit Event
 -- type Input = Maybe Route
 
-data Output = Void
+-- data Output = Void
 
 type OpaqueSlot = H.Slot (Const Unit) Void
 
@@ -61,7 +155,7 @@ _navigation = SProxy :: SProxy "navigation"
 _login = SProxy :: SProxy "login"
 
 type HTML m = H.ComponentHTML Action ChildSlots m
-type ComponentM m eff = H.HalogenM State Action ChildSlots Output m eff
+type ComponentM m eff = H.HalogenM State Action ChildSlots Message m eff
 type QueryHandler m = forall a. Query a -> ComponentM m (Maybe a)
 type ActionHandler m = Action -> ComponentM m Unit
 
@@ -69,9 +163,10 @@ component
   :: forall i m
    . MonadAff m
   => MonadAsk Env m
-  -- => Navigate m
   => WebsocketM m
-  => H.Component HH.HTML Query i Output m
+  => MonadEffect m
+  => Logging m
+  => H.Component HH.HTML Query i Message m
 component = H.mkComponent
   { initialState
   , render
@@ -93,7 +188,22 @@ component = H.mkComponent
     Initialize -> do
       pure unit
 
-    Connect s -> connect s
+    Connect s -> do
+      env <- ask
+      logDebug $ "clicked button " <> env.baseUrl
+    HandleInput text -> do
+      H.modify_ (_ { inputText = text })
+    Submit ev -> do
+      H.liftEffect $ Event.preventDefault ev
+      st <- H.get
+      let outgoingMessage = st.inputText
+      H.raise $ OutputMessage outgoingMessage
+      H.modify_ \st' -> st'
+        { messages = st'.messages `A.snoc` ("Sending: " <> outgoingMessage)
+      , inputText = ""
+      }
+      --   pure unit
+
 
   handleQuery :: QueryHandler m
   handleQuery = case _ of
@@ -106,11 +216,23 @@ component = H.mkComponent
 
   render :: State -> HTML m
   render state =
-    HH.button
-      [ HP.title "hey"
-      , HE.onClick \_ -> Just $ Connect "foo"
-      ]
-      [ HH.text "Socket" ]
+    HH.form
+    [ HE.onSubmit (Just <<< Submit) ]
+    [ HH.ol_ $ map (\msg -> HH.li_ [ HH.text msg ]) state.messages
+    , HH.input
+        [ HP.type_ HP.InputText
+        , HP.value (state.inputText)
+        , HE.onValueInput (Just <<< HandleInput)
+        ]
+    , HH.button
+        [ HP.type_ HP.ButtonSubmit ]
+        [ HH.text "Send Message" ]
+    ]
+    -- HH.button
+    --   [ HP.title "hey"
+    --   , HE.onClick \_ -> Just $ Connect "foo"
+    --   ]
+    --   [ HH.text "Socket" ]
 
 
 
--- src/Main.purs
+++ src/Main.purs
@@ -14,18 +14,20 @@ import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Effect.Console as Console
-import SmsnWeb.Component.Root as Root
 import Example.Driver.Websockets.Log as Log
 import Foreign (F, Foreign, unsafeToForeign, readString)
 import Halogen as H
-import Halogen.HTML as HH
 import Halogen.Aff as HA
+import Halogen.HTML as HH
 import Halogen.VDom.Driver (runUI)
 import SmsnWeb.AppM (runAppM)
+import SmsnWeb.Component.Root as Root
 import SmsnWeb.Env (LogLevel(..), Env)
+import Web.Socket.WebSocket as WS
 
 main :: Effect Unit
 main = do
+  connection <- WS.create "ws://localhost:8182/gremlin" []
 
 
   -- connection <- WS.create "ws://localhost:8182/gremlin" []
@@ -37,15 +39,18 @@ main = do
       env :: Env
       env = { baseUrl, logLevel }
       root = H.hoist (runAppM env) Root.component
+
       -- rootComponent :: H.Component HH.HTML Router.Query {} Void Aff
       -- rootComponent = H.hoist (runAppM environment) Router.component
 
     io <- runUI root unit body
-    pure unit
+
+    io.subscribe $ Root.wsSender connection
+    CR.runProcess (Root.wsProducer connection CR.$$ Root.wsConsumer io.query)
+    -- pure unit
     -- The wsSender consumer subscribes to all output messages
     -- from our component
     -- io.subscribe $ wsSender connection
 
     -- -- Connecting the consumer to the producer initializes both,
     -- -- feeding queries back to our component as messages are received.
-    -- CR.runProcess (wsProducer connection CR.$$ wsConsumer io.query)
